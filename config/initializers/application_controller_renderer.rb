60227bb30d807f5bae9ac48917c034f98256c447
野原晃司 <nohara.koji.infratop@gmail.com>
2020-06-08 18:05:27 +0900

    finish Bookers2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8cc3890c35f9e351cb2f7721faa518dd18261276bc1da5f13f1727a84d262ef33ada26b9670be0bb66f615b6c615cff892204d449c894b4de5677820370245a4'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get '/home/about' => 'homes#about'
+end
